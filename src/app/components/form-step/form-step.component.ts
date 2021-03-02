df93c884c30c70eb0197d07775270e2fb77c8aed
Camilo <camiloguio@gmail.com>
2021-03-02 17:20:05 -0500

    Cambio de Autenticacion

--- src/app/components/form-step/form-step.component.ts
+++ src/app/components/form-step/form-step.component.ts
@@ -44,33 +44,6 @@ export class FormStepComponent{
   min = this.env.min;
   minF = this.env.minF;
 
-  contacto:ContactoInterface = {
-    DatosBasicos: {
-      TipoDocumento: null,  
-      NumeroDocumento: null,  
-      Nombre1: null,  
-      Celular: null,  
-      CorreoPersonal: null
-    },
-  
-    DatosFinancieros: {  
-      ActividadEconomica: null,  
-      ActividadIndependiente: 3,  
-      IngresoMensual: null  
-    },
-  
-    OtrosDatos: {  
-      AutorizaConsultaCentrales: false,  
-      AutorizaMareigua: false,  
-      ValorFinanciar: null,
-      IdentificacionVendedor: null  
-    },
-
-    DatosVehiculo: {
-      Marca: 9
-    }
-  }
-
   /* Functions */
 
   patternCoincide(event, value) {
@@ -82,30 +55,30 @@ export class FormStepComponent{
   }
 
   chechedc(this){
-    this.contacto.OtrosDatos.AutorizaMareigua = true;
+    this.centrales.contacto.OtrosDatos.AutorizaMareigua = true;
   }
 
   sendCentrales(this){
     this.editable = false;
     
-    if(this.contacto.DatosFinancieros.ActividadEconomica){
-      if(this.contacto.DatosFinancieros.ActividadEconomica === 1){
-          this.contacto.DatosFinancieros.ActividadEconomica = 1;
-          this.contacto.DatosFinancieros.ActividadIndependiente = 15;
+    if(this.centrales.contacto.DatosFinancieros.ActividadEconomica){
+      if(this.centrales.contacto.DatosFinancieros.ActividadEconomica === 1){
+          this.centrales.contacto.DatosFinancieros.ActividadEconomica = 1;
+          this.centrales.contacto.DatosFinancieros.ActividadIndependiente = 15;
       }
-      if(this.contacto.DatosFinancieros.ActividadEconomica === 11){
-          this.contacto.DatosFinancieros.ActividadEconomica = 1;
-          this.contacto.DatosFinancieros.ActividadIndependiente = 16;
+      if(this.centrales.contacto.DatosFinancieros.ActividadEconomica === 11){
+          this.centrales.contacto.DatosFinancieros.ActividadEconomica = 1;
+          this.centrales.contacto.DatosFinancieros.ActividadIndependiente = 16;
       }
-      if(this.contacto.DatosFinancieros.ActividadEconomica === 2){
-          this.contacto.DatosFinancieros.ActividadEconomica = 2;
-          this.contacto.DatosFinancieros.ActividadIndependiente = 3;
+      if(this.centrales.contacto.DatosFinancieros.ActividadEconomica === 2){
+          this.centrales.contacto.DatosFinancieros.ActividadEconomica = 2;
+          this.centrales.contacto.DatosFinancieros.ActividadIndependiente = 3;
       }
     }
 
-    this.centrales.authenticate(this.contacto);
+    this.centrales.authenticate();
     setTimeout(() => {
-      this.centrales.response(this.contacto).subscribe((resp:any) => {
+      this.centrales.response(this.centrales.contacto).subscribe((resp:any) => {
         this.respuesta = resp.IdResultado;
         
         if(this.respuesta == 2 || this.respuesta == 3){
@@ -125,8 +98,8 @@ export class FormStepComponent{
 
    checkTyc(this){
     this.modal=false; 
-    this.contacto.OtrosDatos.AutorizaConsultaCentrales=true;
-    this.contacto.OtrosDatos.AutorizaMareigua=true;
+    this.centrales.contacto.OtrosDatos.AutorizaConsultaCentrales=true;
+    this.centrales.contacto.OtrosDatos.AutorizaMareigua=true;
    }
 
    reload()
@@ -144,7 +117,7 @@ changeButtonCliente(val) {
   
 
   const nmv = 0.0115;
-  this.contacto.OtrosDatos.ValorFinanciar = this.valorFinanciarCop;
+  this.centrales.contacto.OtrosDatos.ValorFinanciar = this.valorFinanciarCop;
   let cuota;
   if (val !== undefined) {
 
@@ -177,42 +150,4 @@ changeButtonCliente(val) {
 }
 
   
-}
-
-export interface DatosBasicos {
-  
-  Nombre1?: String; 
-  TipoDocumento?: String;  
-  NumeroDocumento?: String;  
-  Celular?: String;  
-  CorreoPersonal?: String;
-}
-
-export interface DatosFinancieros {
-  
-  ActividadEconomica?: Number;  
-  ActividadIndependiente?: Number;  
-  IngresoMensual?: Number;
-  
-}
-
-export interface OtrosDatos {
-  
-  AutorizaConsultaCentrales?: Boolean;  
-  AutorizaMareigua?: Boolean;  
-  ValorFinanciar?: Number;
-  IdentificacionVendedor?: Number;
-}
-
-export interface DatosVehiculo {
-  
-  Marca: number;
-}
-
-export interface ContactoInterface{
-
-  DatosBasicos?:DatosBasicos;
-  DatosFinancieros?:DatosFinancieros;
-  OtrosDatos?:OtrosDatos;
-  DatosVehiculo:DatosVehiculo;
 }
